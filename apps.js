1bca912f059f89e3c854cf39843b3554b1c167eb
Marwan Reyad AL-Khawaldeh <marwanreyad@gmail.com>
2021-02-10 10:39:15 +0200

    Update

--- apps.js
+++ apps.js
@@ -1,8 +1,8 @@
 
             var fname=prompt('please enter your name','')  ;
             var age = Number(prompt('Please enter your age'));
-
-            if(17 <= age && age  < 35){
+function gitAge(){
+            if(17 <= age && age  <= 35){
                 alert('he is can be a student');
                 
         
@@ -11,37 +11,49 @@
                 alert('he is can not be a student');
                 
             }
-
+            
+}
+gitAge();
 
             var order=prompt("What do you want BMW or Mercedes-Benz");
+            
         while (order!=="BMW" && order!=="Mercedes-Benz"){
             order=prompt("What do you want BMW or Mercedes-Benz")
         }
 
         var item='';
+        function gitCar(){
+
+        
         if(order=='BMW'){
             
             item= '<img src="img/BMW.jpg"/>';
         }else {
              item='<img src="img/Mer.jpg"/>';
         }
+        document.write(item);
+    }
+    gitCar();
 
+    function gitNum(){
         var NumOfCar=prompt("What it is the Number of Cars");
         var result='';
         for(var i=0;i<NumOfCar;i++){
             result=result+item;
         }
         document.write(result);
+        
+    }
+    gitNum();
 
 
-        var area;
+
+        
         function Area(width,hight){
             return width*hight;
         }
-        area=Area(5,6);
-
-
-        // var resultf = `<p>${area}</p>`
+        var area=Area(5,6);
+       // var resultf = `<p>${area}</p>`
         var resultf = '<p>'+'<b>'+'<b>'+"The Area = "+'</b>'+ area +'</b>'+ '</p>'
         document.write(resultf);
 
