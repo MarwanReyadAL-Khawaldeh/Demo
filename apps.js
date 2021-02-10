4aedadf7f3863ff5ffddc58652ec569ee1301405
Marwan Reyad AL-Khawaldeh <marwanreyad@gmail.com>
2021-02-10 11:01:01 +0200

    Update

--- apps.js
+++ apps.js
@@ -1,66 +1,69 @@
 
-            var fname=prompt('please enter your name','')  ;
-            var age = Number(prompt('Please enter your age'));
-function gitAge(){
-            if(17 <= age && age  <= 35){
-                alert('he is can be a student');
-                
-        
-            }
-            else{
-                alert('he is can not be a student');
-                
-            }
-            
+var fname = prompt('please enter your name', '');
+var age = Number(prompt('Please enter your age'));
+function gitAge() {
+    if (17 <= age && age <= 35) {
+        alert('he is can be a student');
+
+
+    }
+    else {
+        alert('he is can not be a student');
+
+    }
+
 }
 gitAge();
 
-            var order=prompt("What do you want BMW or Mercedes-Benz");
-            
-        while (order!=="BMW" && order!=="Mercedes-Benz"){
-            order=prompt("What do you want BMW or Mercedes-Benz")
-        }
+var order = prompt("What do you want BMW or Mercedes-Benz");
 
-        var item='';
-        function gitCar(){
+while (order !== "BMW" && order !== "Mercedes-Benz") {
+    order = prompt("What do you want BMW or Mercedes-Benz")
+}
 
-        
-        if(order=='BMW'){
-            
-            item= '<img src="img/BMW.jpg"/>';
-        }else {
-             item='<img src="img/Mer.jpg"/>';
-        }
-        document.write(item);
+var item = '';
+function gitCar() {
+
+
+    if (order == 'BMW') {
+
+        item = '<img src="img/BMW.jpg"/>';
+    } else {
+        item = '<img src="img/Mer.jpg"/>';
     }
-    gitCar();
+    //document.write(item);
+}
 
-    function gitNum(){
-        var NumOfCar=prompt("What it is the Number of Cars");
-        var result='';
-        for(var i=0;i<NumOfCar;i++){
-            result=result+item;
-        }
-        document.write(result);
+gitCar();
+
+
+function gitNum() {
+    var result = '';
+    var NumOfCar = prompt("What it is the Number of Cars");
+    for (var i = 0; i < NumOfCar; i++) {
         
+        result = result + item;
     }
-    gitNum();
+    document.write(result);
+
+}
+gitNum();
 
 
 
-        
-        function Area(width,hight){
-            return width*hight;
-        }
-        var area=Area(5,6);
-       // var resultf = `<p>${area}</p>`
-        var resultf = '<p>'+'<b>'+'<b>'+"The Area = "+'</b>'+ area +'</b>'+ '</p>'
-        document.write(resultf);
 
-        
+function Area(width, hight) {
+    return width * hight;
+}
+var area = Area(5, 6);
+// var resultf = `<p>${area}</p>`
+var resultf = '<p>' + '<b>' + '<b>' + "The Area = " + '</b>' + area + '</b>' + '</p>'
+document.write(resultf);
 
 
-            
 
-        
-        
+
+
+
+
+
